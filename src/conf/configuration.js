3c977963f3cbe159251c2b8388f6f29f8104b84c
Adrian <adrianfdez469@gmail.com>
2020-08-05 15:37:54 -0500

    Fixing Bug

--- src/conf/configuration.js
+++ src/conf/configuration.js
@@ -1,8 +1,8 @@
 const DEV = {
-    server: "http://localhost:3001/"
+    server: "http://localhost:3001"
 }
 const PROD = {
-    server: "https://shut-app-back.herokuapp.com/"
+    server: "https://shut-app-back.herokuapp.com"
 }
 
 export const DEFAULT_CONFIG = process.env.NODE_ENV === "development" ? DEV : PROD;

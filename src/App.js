08f55ba6457b67f53585d5278c5c516e966634ee
Adrian <adrianfdez469@gmail.com>
2020-08-16 03:11:55 -0500

    mismo commit anterior

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
-import React from 'react';
-import {useRecoilValue} from 'recoil';
+import React, { useEffect } from 'react';
+import {useRecoilValue, useRecoilState} from 'recoil';
 import {view, subscribeToEventsState, loginData} from './components/recoil/atoms';
-
+import axios from 'axios';
+import {DEFAULT_CONFIG} from './conf/configuration';
 /*import Login from './components/login/login';
 import Contacts from './components/contacts/contacts';
 import Chat from './components/chat/chat';
@@ -11,10 +12,11 @@ import './App.css';
 import Backdrop from './components/backdrop/backdrop';*/
 
 // --------------------------- New Style --------------------- //
-import {Switch, Route, Redirect} from 'react-router-dom';
+import {Switch, Route, Redirect, Router} from 'react-router-dom';
 import CssBaseline from '@material-ui/core/CssBaseline';
 import Signin from './view/signin';
 import ContactList from './view/contacts';
+import MainButton from './view/mainButton';
 import MainContainter from './view/main';
 import Toolbar from './view/toolbar';
 import ChatCmp from './view/chat';
@@ -27,45 +29,53 @@ import UserDisconnectSubscriber from './components/events/userDisconnectSubscrib
 import IncomingMsgSubscriber from './components/events/incomingMsgSubscriber';
 
 
-function App() {
+
+const App = props => {
   //const viewState = useRecoilValue(view.getAtom);
-  const subscribe = useRecoilValue(subscribeToEventsState);
+  //const subscribe = useRecoilValue(subscribeToEventsState);
   //const cmp = viewState === view.posibleViews.LOGIN ? <Login /> : viewState === view.posibleViews.CONTACTS ? <Contacts /> : <Chat />;
 
   const userData = useRecoilValue(loginData);
 
-  let subscriptions = null;
-  if(subscribe){
-    subscriptions = <>
-      <NewUserSubscriber />
-      <UserDisconnectSubscriber />
-      {<IncomingMsgSubscriber />}
-    </>
-  }
+  const subscriptions = <>
+    <NewUserSubscriber />
+    <UserDisconnectSubscriber />
+    {<IncomingMsgSubscriber />}
+  </>
+  
+
+  
+  
+  const cmp  = userData !== null 
+  ? <>
+      <Route path="/contacts" exact render={ () => (
+        <>
+          <Toolbar />
+          <MainButton />
+          <ContactList />
+          
+        </>
+      )} />
+      <Route path="/contacts/:id" exact component={ChatCmp} />
+      {subscriptions}
+    </> 
+  : null ;
+
 
   return (
     <MainContainter>
       <CssBaseline />
       <UiComponents />
+      
       <Switch>
         <Route path="/" exact component={Signin} />
         <Route path="/signup" exact component={Signup} />
-        {userData !== null ? <>
-        <Route path="/contacts" exact render={ () => (
-          <>
-            <Toolbar />
-            <ContactList />
-          </>
-        )} />
-        <Route path="/contacts/:id" exact component={ChatCmp} />
-        </> : null }
+        {cmp}
         <Route path="/changepass/:token" exact component={ChangePass} />
-        <Route path="/activateuser/:token/:nickname" exact component={ActivateUser} />
-        <Redirect to="/" />
-        </Switch>
-      
-      
-      {subscriptions}
+        <Route path="/activateuser/:token/:nickname" exact component={ActivateUser} /> 
+        <Redirect from='/' to='/' />       
+      </Switch>
+
     </MainContainter>
   );
 

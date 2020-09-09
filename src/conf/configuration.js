dec241b99f7f861a575daaf1d23969b4d6add70b
Adrian <adrianfdez469@gmail.com>
2020-09-08 23:09:05 -0500

    Volviendo a arreglar la dir del backend que estaba ☻

--- src/conf/configuration.js
+++ src/conf/configuration.js
@@ -4,7 +4,7 @@ const DEV = {
 const PROD = {
     // eslint-disable-next-line no-restricted-globals
     //server: location.origin //"https://shut-app-back.herokuapp.com"
-    server: "https://shut-app-back.herokuapp.com"
+    server: "https://shut-app-pro.herokuapp.com"
 }
 
 export const DEFAULT_CONFIG = process.env.NODE_ENV === "development" ? DEV : PROD;

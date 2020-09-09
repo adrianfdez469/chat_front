092b91d2a49a1c4a88256a16d01713701b991c7f
Adrian <adrianfdez469@gmail.com>
2020-09-08 21:25:28 -0500

    Updates

---
+++ precache-manifest.c55ea4bd5186396ae1faa64119c57dc3.js
@@ -0,0 +1,90 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "92b5ffb965608440e6bad0eb30e6d1a7",
+    "url": "/chat_front/index.html"
+  },
+  {
+    "revision": "20ccf5dc92893565241d",
+    "url": "/chat_front/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "34d55819ddf14b5c2277",
+    "url": "/chat_front/static/js/0.7c0d478b.chunk.js"
+  },
+  {
+    "revision": "b4e9bfab70e3172dde3f42e6c2f1f700",
+    "url": "/chat_front/static/js/0.7c0d478b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6dd2fcbf63b8939cca93",
+    "url": "/chat_front/static/js/1.56458180.chunk.js"
+  },
+  {
+    "revision": "fd7525d544dd9c67d07855cb8778e590",
+    "url": "/chat_front/static/js/1.56458180.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c67fba9c9d17997e3d6e",
+    "url": "/chat_front/static/js/10.b0552e0e.chunk.js"
+  },
+  {
+    "revision": "bfc5c05ccff3d0e3491f",
+    "url": "/chat_front/static/js/11.a549f439.chunk.js"
+  },
+  {
+    "revision": "53bdfd535ff357ec8f83",
+    "url": "/chat_front/static/js/12.8cb38d67.chunk.js"
+  },
+  {
+    "revision": "740d2f23278b503ad5df",
+    "url": "/chat_front/static/js/13.c295f43b.chunk.js"
+  },
+  {
+    "revision": "d0d23d1d82bab4c9852f",
+    "url": "/chat_front/static/js/14.85c386f0.chunk.js"
+  },
+  {
+    "revision": "0ea3f27aaea11fc6408f",
+    "url": "/chat_front/static/js/2.7baee034.chunk.js"
+  },
+  {
+    "revision": "461419ba1b076eb51d76",
+    "url": "/chat_front/static/js/3.31ac1584.chunk.js"
+  },
+  {
+    "revision": "d1e08bfe675bf4dd1478",
+    "url": "/chat_front/static/js/4.77792ecf.chunk.js"
+  },
+  {
+    "revision": "7a6a5282dfb154849b0c",
+    "url": "/chat_front/static/js/7.486c68a0.chunk.js"
+  },
+  {
+    "revision": "24241d38d26a202c24c6e0b4bb290b1d",
+    "url": "/chat_front/static/js/7.486c68a0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cb39dbd8c6e90ddbaea4",
+    "url": "/chat_front/static/js/8.81cf5fc3.chunk.js"
+  },
+  {
+    "revision": "594a9b7481a02e531dd949729c997d26",
+    "url": "/chat_front/static/js/8.81cf5fc3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6a9353a24426ddc2e9b9",
+    "url": "/chat_front/static/js/9.8dcd1a6d.chunk.js"
+  },
+  {
+    "revision": "20ccf5dc92893565241d",
+    "url": "/chat_front/static/js/main.cef111cc.chunk.js"
+  },
+  {
+    "revision": "d8d24a0d23d6393ee072",
+    "url": "/chat_front/static/js/runtime-main.c36e3ba0.js"
+  },
+  {
+    "revision": "551636c39d4b8086cfb0bd5ea02830d3",
+    "url": "/chat_front/static/media/logo192-removebg-preview.551636c3.png"
+  }
+]);

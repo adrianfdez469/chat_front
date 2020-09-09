6b0b28d9040c55e30d79354e9fefac5caeaa5123
Adrian <adrianfdez469@gmail.com>
2020-09-08 22:47:10 -0500

    Arreglando paths

--- src/view/firebaseAuth/firebaseAuth.controller.js
+++ src/view/firebaseAuth/firebaseAuth.controller.js
@@ -70,7 +70,7 @@ const FirebaseAuthController = () => {
             netError={netError}
         />
     }else{
-        return <Redirect to='/app' />
+        return <Redirect to='/chat_front/app' />
     }
     
 

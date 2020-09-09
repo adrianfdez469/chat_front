3c809837c765b17cf397e7faa2ce88d75dba4cc1
Adrian <adrianfdez469@gmail.com>
2020-09-08 21:02:09 -0500

    Configurando variables de entorno
    Llaves del api de firebase

--- src/conf/firebaseConf.js
+++ src/conf/firebaseConf.js
@@ -4,7 +4,15 @@ const firebaseConfig = () => {
         const secrets = require('./secrets');
         return secrets.firebaseConfig;
     }else if(process.env.NODE_ENV === 'production'){
-       return process.env.FIREBASE_CONFIG_CREDENTIALS
+       return {
+        apiKey: process.env.FIREBSAE_API_KEY,
+        authDomain: "shut-up-pro.firebaseapp.com",
+        databaseURL: "https://shut-up-pro.firebaseio.com",
+        projectId: "shut-up-pro",
+        storageBucket: "shut-up-pro.appspot.com",
+        messagingSenderId: "1034880989728",
+        appId: "1:1034880989728:web:574d602e7540573753da45"
+      }
     }
 }
 

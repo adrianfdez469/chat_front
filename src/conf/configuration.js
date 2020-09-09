749fe1e11a992e9a9b377b7b3a4a594e438f3146
Adrian <adrianfdez469@gmail.com>
2020-09-08 22:53:33 -0500

    Arreglando la direccion del api backend

--- src/conf/configuration.js
+++ src/conf/configuration.js
@@ -3,7 +3,8 @@ const DEV = {
 }
 const PROD = {
     // eslint-disable-next-line no-restricted-globals
-    server: location.origin //"https://shut-app-back.herokuapp.com"
+    //server: location.origin //"https://shut-app-back.herokuapp.com"
+    server: "https://shut-app-back.herokuapp.com"
 }
 
 export const DEFAULT_CONFIG = process.env.NODE_ENV === "development" ? DEV : PROD;

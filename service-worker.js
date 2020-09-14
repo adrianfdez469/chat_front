e51efa9b04ce86cd8534a244308f2b0adc17475a
Adrian <adrianfdez469@gmail.com>
2020-09-13 19:08:16 -0500

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/chat_front/precache-manifest.76b15e6182ea88044100c3dfad34f9de.js"
+  "/chat_front/precache-manifest.fbc4715bd20e17dd81144092aaec215d.js"
 );
 
 self.addEventListener('message', (event) => {
